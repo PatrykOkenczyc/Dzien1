f08318fb672ad846a3c8953ec09f00b9114d7f24
Patryk Okenczyc <Xomel@outlook.com>
2024-10-24 11:19:20 +0200

    Add project files.

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Ćwiczenie 1 --- ");
+            Console.WriteLine("Podaj dwie liczby: ");
+            int liczba1 = Convert.ToInt32(Console.ReadLine());
+            int liczba2 = Convert.ToInt32(Console.ReadLine());
+
+            int suma1 = liczba1 + liczba2;
+
+            Console.WriteLine("Liczba 1: " + liczba1);
+            Console.WriteLine("Liczba 2: " + liczba2);
+            Console.WriteLine("Suma: " + suma1);
+            Console.WriteLine("Długość: " + suma1.ToString().Length);
+            Console.ReadKey();
+
+            
+        }
+
+    }
+}
