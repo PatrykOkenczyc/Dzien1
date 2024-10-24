1f772117220786c2e5303b0bf55c43b15b78fa51
Patryk Okenczyc <Xomel@outlook.com>
2024-10-24 11:20:46 +0200

    Ćwiczenie 2

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -23,7 +23,21 @@ namespace Zadanie1
             Console.WriteLine("Długość: " + suma1.ToString().Length);
             Console.ReadKey();
 
-            
+            Console.WriteLine("--- Ćwiczenie 2 --- ");
+            Console.WriteLine("Podaj dwie liczby odzielone spacją: ");
+            string liczby = Console.ReadLine();
+            int pozycja_ = liczby.IndexOf(" ");
+
+            int liczba3 = Convert.ToInt32(liczby.Substring(0, pozycja_));
+            int liczba4 = Convert.ToInt32(liczby.Substring(pozycja_));
+
+            int suma2 = liczba1 + liczba2;
+
+            Console.WriteLine("Liczba 1: " + liczba3);
+            Console.WriteLine("Liczba 2: " + liczba4);
+            Console.WriteLine("Suma: " + suma2);
+            Console.WriteLine("Długość: " + suma2.ToString().Length);
+            Console.ReadKey();
         }
 
     }
