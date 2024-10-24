42245e8e9abb5c0fe2fb5ed77165efedd61a2175
Patryk Okenczyc <Xomel@outlook.com>
2024-10-24 15:54:00 +0200

    Zadanie5

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wage: ");
+            int waga = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj wzrost: ");
+            int wzrost = Convert.ToInt32(Console.ReadLine());           
+
+            double bmi = waga / Math.Pow(Convert.ToDouble(wzrost) / 100, 2); 
+
+            string raport = $"Twoje BMI wynosi: {bmi:F2} co wskazuje ";
+
+            if (bmi < 18.5)
+            {
+                raport += "Niedowagę";
+            }
+            else if (bmi < 24.9)
+            {
+                raport += "Normę";
+            }
+            else if (bmi < 29.9)
+            {
+                raport += "Nadwagę";
+            }
+            else if (bmi < 34.9)
+            {
+                raport += "Otyłość I stopnia";
+            }
+            else if (bmi < 39.9)
+            {
+                raport += "Otyłość II stopnia";
+            }
+            else
+            {
+                raport += "Otyłość III stopnia";
+            }
+            
+            Console.WriteLine(raport);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba_wylosowana = new Random().Next(1, 11);
+            int liczba_uzytkownikanowa = 0;
+            int liczba_uzytkownikastara = int.MaxValue;
+            int ilosc_prob = 0;
+            do
+            {
+                ilosc_prob++;
+                Console.WriteLine("Podaj liczbę od 1 do 10: ");
+                liczba_uzytkownikanowa = Convert.ToInt32(Console.ReadLine());
+                              
+
+                if (Math.Abs(liczba_uzytkownikanowa - liczba_wylosowana) <
+                    Math.Abs(liczba_uzytkownikastara - liczba_wylosowana))
+                    {
+                    Console.WriteLine("Ciepło");
+
+                    }
+                else Console.WriteLine("Zimno");
+                liczba_uzytkownikastara = liczba_uzytkownikanowa;
+
+
+            } while (liczba_wylosowana != liczba_uzytkownikanowa);
+
+            Console.WriteLine($"Gratulacje! Wygrałeś potrzebowałeś {ilosc_prob} prób.");
+            Console.ReadKey();
+        }
+    }
+}
